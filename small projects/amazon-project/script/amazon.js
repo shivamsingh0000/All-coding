9caaecfee608669b8812ddaf33ff192dd4764769
shivamsingh0000 <officialshivamsingh00@gmail.com>
2025-07-11 22:12:18 +0530

    init

--- small projects/amazon-project/script/amazon.js
+++ small projects/amazon-project/script/amazon.js
@@ -47,7 +47,7 @@ productsHTML +=
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
@@ -58,7 +58,26 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
   button.addEventListener('click',() => {
-    console.log(button.dataset.productName);
+    const productId = button.dataset.productId;
+    productId;
+
+      let matchingItem;
+
+    cart.forEach((item) =>{
+      if (productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+    }else{
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+    console.log(cart)
   })
 })
 
