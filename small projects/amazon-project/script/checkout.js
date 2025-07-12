f2f1ceace32fd0f425dc607f418311cdebd5c441
shivamsingh0000 <officialshivamsingh00@gmail.com>
2025-07-12 12:32:20 +0530

    init

--- small projects/amazon-project/script/checkout.js
+++ small projects/amazon-project/script/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 // Declare this outside the loop so it accumulates all items
@@ -35,7 +35,7 @@ cart.forEach((cartItem) => {
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">Update</span>
-            <span class="delete-quantity-link link-primary">Delete</span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id = '${matchingProduct.id}'>Delete</span>
           </div>
         </div>
 
@@ -75,3 +75,14 @@ cart.forEach((cartItem) => {
 
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link)=>{
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+  })
+})
