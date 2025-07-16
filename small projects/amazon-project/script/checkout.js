38ff43b8cdc8b4640cd34c1390b25ae0970c823e
shivamsingh0000 <officialshivamsingh00@gmail.com>
2025-07-16 10:57:07 +0530

    init

--- small projects/amazon-project/script/checkout.js
+++ small projects/amazon-project/script/checkout.js
@@ -1,4 +1,5 @@
 import { renderPaymentSummary }from"./checkout/paymentSummary.js"
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import'../data/cart-oop.js'
 renderOrderSummary();
 renderPaymentSummary();
